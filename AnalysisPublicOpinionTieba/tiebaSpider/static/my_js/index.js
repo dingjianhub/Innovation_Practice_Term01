d8533a13e4c8742260ad275e1dcb696f2e007d8f
dingjianhub <43640148+dingjianhub@users.noreply.github.com>
2019-05-23 23:38:53 +0800

    upload

--- AnalysisPublicOpinionTieba/tiebaSpider/static/my_js/index.js
+++ AnalysisPublicOpinionTieba/tiebaSpider/static/my_js/index.js
@@ -21,10 +21,10 @@ function searchInfo(key) {
     // 首先清空result中的内容以便内容填入
     $("#result").empty();
     $.getJSON({
-        url: "http://localhost:5000/show?page=1&keyword=" + key,
+        url: "http://localhost:8080/info?page=1&keyword=杭州电子科技大学",
         success: function (result) {
             // 获取返回的数据中我们需要的部分
-            res = result.response.docs;
+            res = result.response.toJSON();
             // 利用for插入每一个结果
             if (res.length) {
                 for (i = 0; i < res.length; i++) {
@@ -35,9 +35,9 @@ function searchInfo(key) {
                             <div class='card mb-12 shadow-sm'>
                                 <div class='card-body'>
                                     <h5>` +
-                        res[i].authorName +
-                        ` <small style='margin-left: 10px'>` +
                         res[i].firstFloorContent +
+                        ` <small style='margin-left: 10px'>` +
+                        res[i].href +
                         `</small> <small style='margin-left: 10px'>` +
                         res[i].title +
                         `</small></h5>
